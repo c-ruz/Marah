f47d6023abbecb128c409644b6e1e8ae29c81c97
Carlos Ruz <cruz@nursoft.cl>
2025-12-12 13:58:50 -0300

    Changes to ff

--- src/main/scala/controller/GameController.scala
+++ src/main/scala/controller/GameController.scala
@@ -1,7 +1,8 @@
 package controller
 
+import api.types.grid.GridGame
 import api.{ActionResult, Success}
-import api.types.grid.components.{CellEntity, CellEntityAttribute, ScoreView}
+import api.types.grid.components.{Cell, CellEntity, CellEntityAttribute, ScoreView}
 import api.types.stack.StackGame
 import api.types.stack.components.StackCell
 import controller.states.{InitialState, State}
@@ -11,30 +12,27 @@ import model.entities.enemies.Enemy
 import model.entities.characters.{BlackMage, Character, Paladin, WhiteMage}
 import model.turns.TurnScheduler
 
-class GameController extends StackGame {
+class GameController extends GridGame {
   
   var state: State = new InitialState
   
-  private var enemies: List[Enemy] = List(
-    new Enemy("Enemy 1", 100, 10, 10, 10, Some("bahamut.png"))
-  )
-  private var characters: List[Character] = List(new Paladin, new WhiteMage, new BlackMage)
+  val enemy = new Enemy("Enemy 1", 100, 10, 10, 10, Some("bahamut.png"))
 
-  private val scheduler = new TurnScheduler(entities = enemies ++ characters)
+  val paladin = new Paladin
+
+  val whiteMage = new WhiteMage
+
+  val blackMage = new BlackMage
+
+  private val scheduler = new TurnScheduler(entities = List(enemy, paladin, whiteMage, blackMage))
 
   var currentTurn: GameEntity = scheduler.nextTurn()
 
-  /**
-   * Defines the direction the cells will be rendered.
-   * It can be either "vertical" or "horizontal"
-   */
-  def direction: "vertical" | "horizontal" = "horizontal"
 
-  def stack: List[StackCell] = List()
   /** List of [[ScoreView]] to be shown in the bottom menu.
    */
   def score: List[ScoreView] =
-    (enemies ++ characters).map(e => ScoreView(e.name, e.actionBarString))
+    List(enemy, paladin, whiteMage, blackMage).map(e => ScoreView(e.name, e.actionBarString))
 
 
   /** Message for feedback to the user, rendered in the top bar of the
@@ -46,4 +44,18 @@ class GameController extends StackGame {
   /** List of [[Action]] to be shown in the bottom menu of the visualizer.
    */
   def menuActions: List[Action] = List()
+
+  /** Defines the grid to be rendered. First value defines the horizontal span
+   * of the grid, second value defines the vertical span of the grid.
+   */
+  def gridSize: (Int, Int) = (4,3)
+
+  /** List of [[Cell]] to be rendered inside the grid.
+   */
+  def cells: List[Cell] = List(
+    Cell(None, 0, 1, List(CellEntity(name = "Enemy", attributes = List(), actions = List(), img = Some("bahamut.png"))), List(), None),
+    Cell(None, 2, 0, List(CellEntity(paladin.name, List(), List(), paladin.img)), List(), None),
+    Cell(None, 3, 1, List(CellEntity(blackMage.name, List(), List(), blackMage.img)), List(), None),
+    Cell(None, 2, 2, List(CellEntity(whiteMage.name, List(), List(), whiteMage.img)), List(), None)
+  )
 }
